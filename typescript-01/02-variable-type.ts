33c8760db6b760c05e95e6ae47363be38a12ecf7
Raymond <raymond.deleon@fcc-itservices.com>
2020-01-02 17:36:34 +0800

    Initial typescript fundamentals

---
+++ typescript-01/02-variable-type.ts
@@ -0,0 +1,13 @@
+let a:number;
+let b:boolean;
+let c:string;
+let d:any;
+let e:number[] = [1,2,3];
+let f:any[] = [1, '2', false];
+
+const ColorRed = 0;
+const ColorGreen = 0;
+const ColorBlue = 0;
+
+enum Color { Red = 0, Green = 1, Blue = 2 };
+let backgroundColor = Color.Red
